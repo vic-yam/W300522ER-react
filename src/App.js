37e46a9541e1ab22753a800625a30a7be3bda05c
Victor Yampolsky <tsanhan@gmail.com>
2023-03-20 19:53:31 +0200

    OnClick

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@ import './App.css';
 // import ChildComp from './sandbox/components/ChildComponent';
 // import PageHeader from './components/PageHeader';
 // import TypographyComponent from './sandbox/components/TypographyComponent';
-// import Card from './cards/components/card/Card';
-import Cards from './cards/components/Cards'
+// import Cards from './cards/components/Cards'
 import OnClick from './sandbox/events/OnClick';
-import RaisingEvents from './sandbox/events/RaisingEvents';
 
 function App() {
   // const hwString = 'Hello World';
   // const name = { firstName: 'John', lastName: 'Doe' };
   return (
     <div className="App"  >
+      {/* <PageHeader /> */}
+      {/* <TypographyComponent /> */}
+      {/* <ChildComp firstName={name.firstName} lastName={name.lastName}  /> */}
+      {/* <Cards></Cards> */}
       <OnClick></OnClick>
-      <RaisingEvents></RaisingEvents>
     </div>
   );
 }
