d5fcadb1a2d2c8a7ae130f03c18c70b213ac3523
Victor Yampolsky <tsanhan@gmail.com>
2023-03-20 17:49:42 +0200

    HW solution

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import './App.css';
 // import ChildComp from './sandbox/components/ChildComponent';
-import PageHeader from './components/PageHeader';
-import TypographyComponent from './sandbox/components/TypographyComponent';
-import Card from './cards/components/card/Card';
+// import PageHeader from './components/PageHeader';
+// import TypographyComponent from './sandbox/components/TypographyComponent';
+// import Card from './cards/components/card/Card';
+import Cards from './cards/components/Cards'
 
 function App() {
-  const hwString = 'Hello World';
-  const name = { firstName: 'John', lastName: 'Doe' };
+  // const hwString = 'Hello World';
+  // const name = { firstName: 'John', lastName: 'Doe' };
   return (
     <div className="App"  >
       {/* <PageHeader /> */}
       {/* <TypographyComponent /> */}
       {/* <ChildComp firstName={name.firstName} lastName={name.lastName}  /> */}
-      <Card></Card>
+      <Cards></Cards>
     </div>
   );
 }
--- src/cards/components/Cards.jsx
+++ src/cards/components/Cards.jsx
@@ -1,34 +1,70 @@
-// const Cards = ({ cards }) => {
-//     const card = [{
-//         _id: "639221ec70962dd4df2b709b",
-//         title: "Business Name",
-//         subtitle: "Business Headline",
-//         description: "some desc",
-//         phone: "0500000000",
-//         email: "admin@gmail.com",
-//         web: "https://www.hackampus.com",
-//         image: {
-//           url: "https://cdn.pixabay.com/photo/2019/12/18/19/22/christmas-4704706_960_720.jpg",
-//           alt: "Wowww this is Apple Watch",
-//           _id: "639221ec70962dd4df2b709c",
-//         },
-//         address: {
-//           state: "israel",
-//           country: "israel",
-//           city: "Tel Aviv",
-//           street: "STREET",
-//           houseNumber: 1,
-//           zip: 123456,
-//           _id: "639221ec70962dd4df2b709d",
-//         },
-//         bizNumber: 6480165,
-//         createdAt: "2022-12-08T17:42:04.379Z",
-//         user_id: "638503e4caa1f3d9dbbcf7bc",
-//         likes: ["639221ec70962dd4df2b70a3"],
-//         __v: 0,
-//       }];
-//   return (
+import Card from "./card/Card";
 
-//   )
-// };
-// export default Cards;
+const Cards = () => {
+  const cards = [
+    {
+      _id: "639221ec70962dd4df2b709b",
+      title: "Business Name",
+      subtitle: "Business Headline",
+      description: "some desc",
+      phone: "0500000000",
+      email: "admin@gmail.com",
+      web: "https://www.hackampus.com",
+      image: {
+        url: "https://cdn.pixabay.com/photo/2019/12/18/19/22/christmas-4704706_960_720.jpg",
+        alt: "Wowww this is Apple Watch",
+        _id: "639221ec70962dd4df2b709c",
+      },
+      address: {
+        state: "israel",
+        country: "israel",
+        city: "Tel Aviv",
+        street: "STREET",
+        houseNumber: 1,
+        zip: 123456,
+        _id: "639221ec70962dd4df2b709d",
+      },
+      bizNumber: 6480165,
+      createdAt: "2022-12-08T17:42:04.379Z",
+      user_id: "638503e4caa1f3d9dbbcf7bc",
+      likes: ["639221ec70962dd4df2b70a3"],
+      __v: 0,
+    },
+    {
+      _id: "639221ec70962dd4df2b709b",
+      title: "Tesla",
+      subtitle: "Car Company",
+      description: "Innovative Car Company",
+      phone: "0500000000",
+      email: "admin@gmail.com",
+      web: "https://www.hackampus.com",
+      image: {
+        url: "https://cdn.pixabay.com/photo/2019/12/18/19/22/christmas-4704706_960_720.jpg",
+        alt: "Wowww this is Apple Watch",
+        _id: "639221ec70962dd4df2b709c",
+      },
+      address: {
+        state: "israel",
+        country: "israel",
+        city: "Tel Aviv",
+        street: "STREET",
+        houseNumber: 1,
+        zip: 123456,
+        _id: "639221ec70962dd4df2b709d",
+      },
+      bizNumber: 6480165,
+      createdAt: "2022-12-08T17:42:04.379Z",
+      user_id: "638503e4caa1f3d9dbbcf7bc",
+      likes: ["639221ec70962dd4df2b70a3"],
+      __v: 0,
+    },
+  ];
+  return (
+    <>
+      {cards.map((card) => {
+        return <Card key={card._id} card={card} />;
+      })}
+    </>
+  );
+};
+export default Cards;
